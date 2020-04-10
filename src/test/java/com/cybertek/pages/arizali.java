796c1c597a727ca05d9b54311cf713115be27eb3
IBRAHIM ARSLAN <ibrahimarslan35@gmail.com>
2020-04-10 10:07:05 -0400

    sec

--- src/test/java/com/cybertek/pages/arizali.java
+++
@@ -1,7 +0,0 @@
-package com.cybertek.pages;
-
-public class arizli {
-    public static void main(String[] args) {
-
-    }
-}
--- src/test/java/com/cybertek/pages/isa.java
+++ src/test/java/com/cybertek/pages/isa.java
@@ -3,5 +3,6 @@ package com.cybertek.pages;
 public class isa {
     public static void main(String[] args) {
         System.out.println("isa");
+        System.out.println("ibrahim");
     }
 }
