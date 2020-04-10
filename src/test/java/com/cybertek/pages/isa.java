351d62497d76afbb0d007698603091cba07d92c8
ahmetosan <ahmetpcf@gmail.com>
2020-04-10 10:10:11 -0400

    3 changes

--- src/test/java/com/cybertek/pages/isa.java
+++ src/test/java/com/cybertek/pages/isa.java
@@ -4,4 +4,7 @@ public class isa {
     public static void main(String[] args) {
         System.out.println("isa");
     }
+    public void number(){
+        System.out.println("olur ogle");
+    }
 }
