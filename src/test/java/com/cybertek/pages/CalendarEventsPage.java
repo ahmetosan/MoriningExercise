8dea8a8e7d80e86d88694ac159456f32eac17ecc
IBRAHIM ARSLAN <ibrahimarslan35@gmail.com>
2020-04-14 15:05:43 -0400

    sad

--- src/test/java/com/cybertek/pages/CalendarEventsPage.java
+++ src/test/java/com/cybertek/pages/CalendarEventsPage.java
@@ -13,5 +13,6 @@ public class CalendarEventsPage extends BasePage {
 
     @FindBy(css = "[title='Create Calendar event']")
     public WebElement createCalendarEvent;
-
+    @FindBy(xpath = "//tr[contains(.,'Testers Meeting')]/td[contains(.,'...')]")
+    public WebElement threeDot;
 }
