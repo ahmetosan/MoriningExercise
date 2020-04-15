427b328ec333690a0f38126323303903cf1a4b90
selimpcf <58636568+selimpcf@users.noreply.github.com>
2020-04-15 17:23:25 -0500

    WEdnesday

--- src/test/java/com/cybertek/pages/DashboardPage.java
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.PageFactory;
 public class DashboardPage extends BasePage{
     //no need to explicitly write constructors, because it will use its parents constructor.
     public DashboardPage(){
+
         PageFactory.initElements(Driver.get(),this);
     }
 }
--- src/test/java/com/cybertek/tests/Selim1.java
+++ src/test/java/com/cybertek/tests/Selim1.java
@@ -7,6 +7,8 @@ import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Selim1 extends TestBase {
@@ -46,5 +48,62 @@ public class Selim1 extends TestBase {
         System.out.println(delete.isEnabled());
         System.out.println(delete.isSelected());
         System.out.println(delete.isDisplayed());
+    }
+
+    @Test
+    public void testCase2() throws InterruptedException {
+        driver.get(ConfigurationReader.get("qa1_url"));
+        LoginPage loginPage = new LoginPage();
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+        loginPage.login(username, password);
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        Thread.sleep(5000);
+        WebElement element = driver.findElement(By.xpath("//i[@class='fa-cog hide-text']"));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+        Thread.sleep(2000);
+        //tbody/tr[7]/td[3]
+
+        for (int i = 2; i <= 7; i++) {
+            WebElement checkboxes = driver.findElement(By.xpath("//tbody/tr[" + i + "]/td[3]"));
+            if (checkboxes.isDisplayed()) {
+                checkboxes.click();
+            }
+
+        }
+        WebElement title = driver.findElement(By.xpath("//tbody/tr[" + 1 + "]/td[3]"));
+        Assert.assertTrue(title.isDisplayed());
+    }
+
+    @Test
+    public void testCase3() throws InterruptedException {
+        driver.get(ConfigurationReader.get("qa1_url"));
+        LoginPage loginPage = new LoginPage();
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+        loginPage.login(username, password);
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        Thread.sleep(5000);
+        WebElement calEvent = driver.findElement(By.xpath("//a[@title='Create Calendar event']"));
+        wait.until(ExpectedConditions.elementToBeClickable(calEvent));
+        calEvent.click();
+
+        Thread.sleep(3000);
+        WebElement expandButton = driver.findElement(By.xpath("//span[@class='caret']"));
+        wait.until(ExpectedConditions.elementToBeClickable(expandButton));
+        expandButton.click();
+
+        //verify 3 buttons
+        WebElement button1 = driver.findElement(By.xpath("(//*[contains(text(),'Save and Close')])[2]"));
+        WebElement button2 = driver.findElement(By.xpath("//*[contains(text(),'Save and New')]"));
+        WebElement button3 = driver.findElement(By.xpath("//*[contains(text(),'Save')]"));
+        Assert.assertTrue(button1.isDisplayed());
+        Assert.assertTrue(button2.isDisplayed());
+        Assert.assertTrue(button3.isDisplayed());
     }
 }
